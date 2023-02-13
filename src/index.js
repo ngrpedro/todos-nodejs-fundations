64b51ea083df08fe9f325539d627247d7a60ff19
Pedro Soares <ngrpedro22@gmail.com>
2023-02-13 18:56:12 -0300

    feat:: post new todo

--- src/index.js
+++ src/index.js
@@ -12,11 +12,27 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+
+  console.log(user);
+
+  if (!user) {
+    return response.status(400).json({ error: "User not fund!" });
+  }
+  request.user = user;
+  return next();
 }
 
 app.post("/users", (request, response) => {
   // Complete aqui
   const { name, username } = request.body;
+
+  const userAlreadyExists = users.some((user) => user.username === username);
+
+  if (userAlreadyExists) {
+    return response.status(400).json({ error: "User already exists!" });
+  }
 
   users.push({
     id: uuidv4(),
@@ -30,6 +46,9 @@ app.post("/users", (request, response) => {
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { user } = request;
+
+  return response.status(200).json(user.todos)
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
